bc08358f952df04fa71e0053d007c71d2ee357cf
Gabriel Guillon <gguillon@octo.com>
2013-03-08 11:52:05 +0100

    + inclusion tests

--- make.sh
+++ make.sh
@@ -7,11 +7,25 @@ do
 	echo -n "$bn ... "
 	if ./t/$(basename "$i") > logs/$bn.log
 	then
+		# test ok
 		echo "ok. Adding"
 		cat libs/$bn >> $1
 	else
+		# test failed
 		echo "failed"
 	fi
 done
-sed -i '' -e "s/__libname__/$1/" $1
+sed -i  -e "s/__libname__/$1/" $1
 echo "# built on $(date +%Y-%m-%d)" >> $1
+echo "Testing inclusion of :"
+sh "$1" list | awk '{print $1}' | while read f
+do
+	echo -n "> $f"
+	eval "$(sh "$1" "$f")" ; _r=$?
+	if [ $_r -eq 0 ]
+	then
+		echo " ... ok"
+	else
+		echo " ... fail"
+	fi
+done
