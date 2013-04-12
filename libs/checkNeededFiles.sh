cdc8672baa1032c18954fb1b468bb4427a39025e
Gab <gab@korsani.fr>
2013-04-12 17:30:26 +0200

    * which -> type -p

--- libs/checkNeededFiles.sh
+++ libs/checkNeededFiles.sh
@@ -1,12 +1,14 @@
 # Check wether specified file can be found, warn or exit according it's a MAY or a MUST
 # Initialize the variable with the path of the correspondant file, if the file is found
-# Initialize the variable with 'echo <commande>' if the command is not found
+# Initialize the variable with 'echo <commande>' if the file is not found
 # checkNeededFiles may [ file [ file [ ... ] ]
 # checkNeededFiles must [ file [ file [ ... ] ]
-# Example : checkNeededFiles may bash
+# Example :
+# > checkNeededFiles may bash
 # > echo $bash
 # > /bin/bash
-# Example : checkNeededFiles may conntrack
+# Example : 
+# > checkNeededFiles may conntrack # supposing conntrack is is not installed
 # > $conntrack -D -s 1.1.1.1
 # > conntrack -D -s 1.1.1.1
 function checkNeededFiles {
@@ -21,7 +23,7 @@ function checkNeededFiles {
 				type=must
 				;;
 			*)
-				if ! which "$1" >/dev/null 2>&1
+				if ! type -p "$1" >/dev/null 2>&1
 				then
 					[ "$type" = "may" ] && echo "[__libname__] '$1' not found. Bad things may happen" >&2 && _ec=1
 					[ "$type" = "must" ] && echo "[__libname__] '$1' not found. Bad things WILL happen" >&2 && _ec=2
