dc41b34986860c3585bc4523c07a148b05ee1383
Gabriel Guillon <gguillon@octo.com>
2014-03-27 14:50:01 +0100

    * sh -> bash

--- make.sh
+++ make.sh
@@ -20,10 +20,10 @@ sed -i  -e "s/__libname__/$1/" $tlib
 echo "# built on $(date +%Y-%m-%d)" >> $tlib
 mv $tlib $1
 echo "Testing inclusion of :"
-sh $1 list | awk '{print $1}' | while read f
+bash $1 list | awk '{print $1}' | while read f
 do
 	echo -n "> $f"
-	eval "$(sh $1 "$f")" ; _r=$?
+	eval "$(bash $1 "$f")" ; _r=$?
 	if [ $_r -eq 0 ]
 	then
 		echo " ... ok"
