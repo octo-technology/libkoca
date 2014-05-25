fa76b6b3e6ab73d98cbd2b0907157cef22554085
Gabriel Guillon <gguillon@octo.com>
2014-05-25 16:00:19 +0200

    + doc

--- libs/isBackgrounded.sh
+++ libs/isBackgrounded.sh
@@ -1,4 +1,4 @@
-function koca_isBackgrounded() { # Return true is process is backgrounded
+function koca_isBackgrounded() { # Return true is process is backgrounded. Thanks to http://is.gd/4h3fk0
 	case $(ps -o stat= -p $$) in
 		*+*) return 1;;
 		*) return 0;;
