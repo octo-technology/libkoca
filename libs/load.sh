5f961cdb708949746caa7db8ac656b0b24782c3c
Gab <korsani@free.fr>
2014-11-25 11:35:09 +0100

    Bugfix: test failed if wanted load was 0

--- libs/load.sh
+++ libs/load.sh
@@ -1,7 +1,7 @@
 # Return true is load is less or equal to value
 function koca_load() { # Return true if load is less or equals to specified float value
 	thr=$1
-	if [[ $thr =~ ^[0-9]+\.?[0-9]*$ ]]
+	if [[ $thr =~ ^-?[0-9]+\.?[0-9]*$ ]]
 	then
 		return  $(bc <<< "$(cat /proc/loadavg | awk '{printf "%.0f",$1}') > $1")
 	else
--- t/load.sh
+++ t/load.sh
@@ -5,7 +5,7 @@ testload() {
 	assertEquals "Load accepts non int/float" "2" "$r"
 	koca_load 10 ;r=$?
 	assertTrue "Load is less than 10" "$r"
-	koca_load 0 ;r=$?
+	koca_load -1 ;r=$?
 	assertFalse "Load is less than 0" "$r"
 }
 source $(cd $(dirname "$0") ; pwd)/footer.sh
