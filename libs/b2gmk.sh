f641de641fb5aa01d40aa55403e1879be171d153
Gabriel Guillon <gguillon@octo.com>
2016-01-03 00:54:04 +0100

    Symplifying code, and 512000 returns 500ko, not .5Mo

--- libs/b2gmk.sh
+++ libs/b2gmk.sh
@@ -1,26 +1,20 @@
 function koca_b2gmk {	# seconds to day hour min sec
 	w=$1
 	[ -z "$w" ] && read w
-
-	pw=$(echo "scale=1;$w/1125899906842624" | bc)
-	[ "$pw" != "0" ] && echo "${pw}Po" && return
-	w=$(echo "scale=0;$w%1125899906842624" | bc)
-
-	tw=$(echo "scale=1;$w/1099511627776" | bc)
-	[ "$tw" != "0" ] && echo "${tw}To" && return
-	w=$(echo "scale=0;$w%1099511627776" | bc)
-
-	gw=$(echo "scale=1;$w/1073741824" | bc)
-	[ "$gw" != "0" ] && echo "${gw}Go" && return 
-	w=$(echo "scale=0;$w%1073741824" | bc)
-
-	mw=$(echo "scale=1;$w/1048576" | bc)
-	[ "$mw" != "0" ] && echo "${mw}Mo" && return
-	w=$(echo "scale=0;$w%1048576" | bc)
-
-	kw=$(echo "scale=1;$w/1024" | bc)
-	[ "$kw" != "0" ] && echo "${kw}ko" && return
-	w=$(echo "scale=0;$w%1024" | bc)
+	symbols=(o ko Mo Go To Po ) # Eo, Zo and Yo are too big. 'o' is never used ;)
+	for i in $(seq 0 ${#symbols[*]})
+	do
+		values[$i]=$(echo 1024^$i|bc)
+	done
+	for i in $(seq ${#symbols[*]} -1 0)
+	do
+		if [ $w -ge ${values[$i]} ]
+		then
+			pw=$(echo "scale=1;$w/${values[$i]}" | bc)
+			[ "$pw" != "0" ] && echo "${pw}${symbols[$i]}" && return
+		fi
+		w=$(echo "scale=0;$w%${values[$i]}" | bc)
+	done
 
 	echo "${w}"
 }
