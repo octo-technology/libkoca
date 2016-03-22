0787c1d927df8da3672baca1e16201f44c632d50
Gabriel Guillon <gguillon@octo.com>
2016-03-22 22:10:35 +0100

    Added dependecy on gotRoot (*sigh*)

--- libs/dieIfNotRoot.sh
+++ libs/dieIfNotRoot.sh
@@ -1,9 +1,11 @@
 function dieIfNotRoot { # Exit calling script if not running under root
+	local src=__libkoca__ ; [ -e $src ] && eval "$(bash $src gotRoot)"
 	! gotRoot && echo "[__libname__] Actually, I should be run as root" && exit 1
 	#! underSudo && echo "[__libname__] Actually, I should be run under sudo" && exit 1
 	return 0
 }
 function dieIfRoot { # Exit calling script if run under root
+	local src=__libkoca__ ; [ -e $src ] && eval "$(bash $src gotRoot)"
 	gotRoot && echo "[__libname__] I should not be run as root" && exit 1
 	#underSudo && echo "[__libname__] I should not be run under sudo" && exit 1
 	return 0
